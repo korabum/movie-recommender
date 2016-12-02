40ab2f3888a8121351c3bd993d09ff7a73ba0efe
korabum <rizkiman@yahoo.co.id>
2016-12-02 21:27:53 +0700

    Compute cosine similarity

--- preprocessplot.py
+++ preprocessplot.py
@@ -3,6 +3,7 @@ import sys
 from stop_words import get_stop_words
 import string
 from nltk.stem import PorterStemmer
+from numpy import log10
 
 stop_words = get_stop_words('en')
 ps = PorterStemmer()
@@ -20,18 +21,38 @@ def removeStopWords(text):
 def removePunctuation(text):
 	return text.translate(None, string.punctuation)
 
-def stem(text):
-	words = text.split()
-	stemmed_words = [ps.stem(word) for word in words]
-	return ' '.join(stemmed_words)
-
+def stem(word):
+	return ps.stem(word)
 
 if __name__ == "__main__":
 	titles = r.smembers('movies')
+	number_of_movies = len(titles)
+	df = {}
+	tf = {}
+	hehe = 0
 	for title in titles:
-	    movie = r.hgetall('movie:' + title)
-	    plot = movie['plot']
-	    processed_plot = removePunctuation(plot)
-	    processed_plot = removeStopWords(processed_plot)
-	    processed_plot = stem(processed_plot)
-	    r.hset('process_movie:' + title, 'plot', processed_plot)
+		hehe += 1
+		print hehe
+		tf[title] = {}
+		plot = r.hget('movie:' + title, 'plot')
+		processed_plot = removePunctuation(plot)
+		# processed_plot = removeStopWords(processed_plot)
+		for word in processed_plot.split():
+			if word in stop_words:
+				continue
+			word = stem(word)
+			if word not in df:
+				df[word] = {}
+			df[word][title] = True
+
+			if word not in tf[title]:
+				tf[title][word] = 0
+			tf[title][word] += 1
+
+	for word in df:
+		df[word] = sum(df[word].values())
+
+	for title in tf:
+		for word in tf[title]:
+			weight = tf[title][word] * log10(number_of_movies / df[word])
+			r.hset('weight:' + title, word, weight)
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from math import sqrt
 import redis
 import sys
 
@@ -8,9 +9,27 @@ sys.setdefaultencoding('iso-8859-1')
 app = Flask(__name__)
 r = redis.StrictRedis(host='localhost', port=6379, db=0, encoding='iso-8859-1')
 
+titles = r.smembers('movies')
+weight = {}
+vector_length = {}
+for title in titles:
+    weight[title] = r.hgetall('weight:' + title)
+
+for title in weight:
+    total = 0
+    for word in weight[title]:
+        total += weight[title][word] ** 2
+    vector_length[title] = sqrt(total)
+
+def get_cosine_similarity(first_title, second_title):
+    total = 0
+    for word in weight[first_title]:
+        if word in weight[second_title]:
+            total += weight[first_title][word] * weight[second_title][word]
+    return total / (vector_length[first_title] * vector_length[second_title])
+
 @app.route('/get-plot-summary/<query>')
 def get_plot_summary(query):
-    titles = r.smembers('movies')
     matches = []
     standardized_query = query.lower()
     words = standardized_query.split()
@@ -40,6 +59,21 @@ def get_plot_summary(query):
             movies.append({'title': title})
         ret = {'status': 1, 'movies': movies}
         return jsonify(ret)
+
+@app.route('/get-similar-movies/<query>')
+def get_similar_movies(query):
+    max_similarity = 0
+    similar_title = ""
+    for title in titles:
+        if title == query:
+            continue
+        similarity = get_cosine_similarity(query, title)
+        if similarity > max_similarity:
+            max_similarity = similarity
+            similar_title = title
+    plot = r.hget('movie:' + similar_title, 'plot')
+    ret = {'status': 0, 'title': similar_title, 'plot': plot}
+    return jsonify(ret)
 
 if __name__ == "__main__":
     app.run()
