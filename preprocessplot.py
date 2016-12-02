6b351105af048f9c29e787866e17ba3cf00d8130
kemasm <maxx23295@gmail.com>
2016-12-02 19:48:19 +0700

    add stemmer

---
+++ preprocessplot.py
@@ -0,0 +1,32 @@
+import redis
+import sys
+from stop_words import get_stop_words
+import string
+from nltk.stem import PorterStemmer
+
+stop_words = get_stop_words('en')
+stemmer = 
+
+reload(sys)
+sys.setdefaultencoding('iso-8859-1')
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0, encoding='iso-8859-1')
+
+def removeStopWords(text):
+	removed = text.lower()
+	removed = ' '.join([word for word in removed.split() if word not in stop_words])
+	return removed
+
+def removePunctuation(text):
+	return text.translate(None, string.punctuation)
+
+def stem(text):
+
+
+titles = r.smembers('movies')
+for title in titles:
+    movie = r.hgetall('movie:' + title)
+    plot = movie['plot']
+    processed_plot = removePunctuation(plot)
+    processed_plot = removeStopWords(plot)
+    r.hset('process_movie:' + title, 'plot', processed_plot)
