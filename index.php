157e99c831e4f533b56fd73af62e1a2b2e80315f
3kus <qwerty_keypad@live.com>
2016-12-13 17:53:04 +0700

    try frontend 2

--- index.php
+++ index.php
@@ -7,9 +7,24 @@ if (isset($_GET['title'])) {
 
 	// $content = file_get_contents('./status0');
 	// $json = json_decode($content, true);
+
 	if ($json["status"] == 0) {
-		$plot = $json["plot"];
-		$title = $json["title"];
+		$main_title = $json["title"];
+		$main_plot = $json["plot"];
+
+		$url = './get_similar_movies/' . $_POST["title"];
+		$content = file_get_contents($url);
+		$json = json_decode($content, true);
+
+		// $content = file_get_contents('./output');
+		// $json = json_decode($content, true);
+
+		if ($json["status"] == 0) {
+			$similar_movies = $json["movies"];
+		} else if ($json["status"] == 404) {
+			print_r("Not Found");
+		}
+
 	} else if ($json["status"] == 1) {
 		$movies = $json["movies"];
 	} else if ($json["status"] == 404) {
@@ -19,17 +34,7 @@ if (isset($_GET['title'])) {
 }
 
 if (isset($_POST['similar_title'])) {
-	$url = './get_similar_movies/' . $_POST["title"];
-	$content = file_get_contents($url);
-	$json = json_decode($content, true);
-
-	// $content = file_get_contents('./output');
-	// $json = json_decode($content, true);
-	if ($json["status"] == 0) {
-		$similar_movies = $json["movies"];
-	} else if ($json["status"] == 404) {
-		print_r("Not Found");
-	}
+	
 }
 
 ?>
@@ -52,14 +57,17 @@ if (isset($_POST['similar_title'])) {
 </div>
 
 <?php
-	if (isset($title)) {
-		echo "Title: ".$title."<br>";
-		echo "Plot summary:<br>".$plot;
-		echo '<form action="" method="post">
-			<input type="hidden" value="'.$title.'" name="similar_title">
-			<input type="submit" value="Get Similar Movies">
-			</form>';
+	if (isset($main_title)) {
+		echo "Title: ".$main_title."<br>";
+		echo "Plot summary:<br>".$main_plot."<br>";
+		echo "Similar Movies:<br>";
 
+		echo "<ul>";
+		foreach ($similar_movies as $movie) {
+			echo '<li>Title: '.$movie["title"].'<br>
+			Plot summary: '.$movie["plot"].'</li>';
+		}
+		echo "</ul>";
 	}
 
 	if (isset($movies)) {
@@ -68,15 +76,6 @@ if (isset($_POST['similar_title'])) {
 			echo '<li>
 			<a href="./?title='.$movie["title"].'">'.$movie["title"].'</a>
 			</li>';
-		}
-		echo "</ul>";
-	}
-
-	if (isset($similar_movies)) {
-		echo "<ul>";
-		foreach ($similar_movies as $movie) {
-			echo '<li>Title: '.$movie["title"].'<br>
-			Plot summary: '.$movie["plot"].'</li>';
 		}
 		echo "</ul>";
 	}
